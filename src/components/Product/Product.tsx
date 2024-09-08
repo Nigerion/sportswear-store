539c5fcb40e09b8255e6491befbd3a625bc2851a
Антон <115921794+Nigerion@users.noreply.github.com>
2024-09-08 15:31:01 +0400

    создал структуру

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Product.module.css";
+type Props = {};
+
+const Product = (props: Props) => {
+  return <div>Product</div>;
+};
+
+export default Product;
